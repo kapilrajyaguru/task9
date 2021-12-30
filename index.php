eaf8c58a73526174a3094a33b499b6b546e36a1c
Kapil Rajyaguru <kapil@localhost.localdomain>
2021-12-30 12:45:32 -0800

    third change

--- index.php
+++ index.php
@@ -11,6 +11,6 @@
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
 		<p> <?php phpinfo(); ?> </p> 
-        <h2> HELLO FOLKS</h2>
+        <h2> HELLO FOLKS How are you</h2>
 	</body> 
 </html>
